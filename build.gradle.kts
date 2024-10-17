9ed01ff84f9df631a225536760391206f0fd011e
ev chang <wyvestbusiness@gmail.com>
2024-10-16 21:35:41 -0400

    Revert "simplification and bump to 3.3.3"
    This reverts commit d00b80267da12c2eb16cc587f9e55c93819f415e.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
+    id("cc.polyfrost.multi-version")
+    id("cc.polyfrost.defaults.repo")
+    id("cc.polyfrost.defaults.java")
+    id("cc.polyfrost.defaults.loom")
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("maven-publish")
     id("signing")
@@ -6,7 +10,9 @@ plugins {
 }
 
 group = "cc.polyfrost"
-version = "1.0.0-alpha27"
+version = "1.0.0-alpha26"
+
+val noArm = project.name.contains("noarm")
 
 base {
     archivesName.set("lwjgl-${project.name}")
@@ -19,18 +25,62 @@ repositories {
 val shade: Configuration by configurations.creating
 
 dependencies {
-    val version = "3.3.3"
-    val platforms = listOf("windows", "windows-x86", "linux", "macos", "macos-arm64")
-    val dependencies = mutableListOf("-nanovg")
-    if (project.name == "legacy") {
-        dependencies += listOf("-stb", "-tinyfd", "")
+    val lwjglVersion = when (platform.mcVersion) {
+        in 10809..11202 -> "3.3.1"
+        in 11203..11802 -> if (noArm) "3.2.1" else "3.3.1"
+        else -> "3.3.1"
     }
 
-    for (dep in dependencies) {
-        val dependency = "org.lwjgl:lwjgl$dep:$version"
-        shade(dependency)
-        for (platform in platforms) {
-            shade("$dependency:natives-$platform")
+    if (platform.mcVersion <= 11202) {
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion")
+
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion:natives-windows")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:natives-windows")
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion:natives-windows-x86")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:natives-windows-x86")
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion:natives-windows-arm64")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:natives-windows-arm64")
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion:natives-linux")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:natives-linux")
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion:natives-macos")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:natives-macos")
+        shade("org.lwjgl:lwjgl-stb:$lwjglVersion:natives-macos-arm64")
+        shade("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:natives-macos-arm64")
+
+        shade("org.lwjgl:lwjgl:$lwjglVersion")
+        shade("org.lwjgl:lwjgl:$lwjglVersion:natives-windows")
+        shade("org.lwjgl:lwjgl:$lwjglVersion:natives-windows-x86")
+        shade("org.lwjgl:lwjgl:$lwjglVersion:natives-windows-arm64")
+        shade("org.lwjgl:lwjgl:$lwjglVersion:natives-linux")
+        shade("org.lwjgl:lwjgl:$lwjglVersion:natives-macos")
+        shade("org.lwjgl:lwjgl:$lwjglVersion:natives-macos-arm64")
+    }
+
+    shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion") {
+        isTransitive = false
+    }
+    if (platform.mcVersion !in 11203..11802 || noArm) {
+        shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion:natives-windows") {
+            isTransitive = false
+        }
+        shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion:natives-linux") {
+            isTransitive = false
+        }
+        shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion:natives-macos") {
+            isTransitive = false
+        }
+    }
+
+    if (lwjglVersion != "3.2.1") {
+        shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion:natives-macos-arm64") {
+            isTransitive = false
+        }
+        shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion:natives-windows-x86") {
+            isTransitive = false
+        }
+        shade("org.lwjgl:lwjgl-nanovg:$lwjglVersion:natives-windows-arm64") {
+            isTransitive = false
         }
     }
 }
